9eb04883e951dd7caf6b6c5b120caa6df33d143c
InshutiSouvede <isouvede2@gmail.com>
2024-10-08 16:51:42 +0200

    Custom logger ovorriding the console.log, .error,...

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { DatabaseModule } from './database/database.module';
+import { MyLoggerModule } from './my-logger/my-logger.module';
 
 @Module({
-  imports: [EmployeesModule, DatabaseModule],
+  imports: [EmployeesModule, DatabaseModule, MyLoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MyLoggerService } from './my-logger/my-logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useLogger(app.get(MyLoggerService));
   await app.listen(3000);
 }
 bootstrap();
